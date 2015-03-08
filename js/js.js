d0b7e8414dabac0d79b56476075d5562373dcebc
Ruben Del Rio <rbndelrio@gmail.com>
2015-03-08 19:53:35 -0400

    Brewery page: Images in place, IMG now treated as a bg cover, life is beautiful

--- js/js.js
+++ js/js.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
-	console.log('Hello, World!');
+	var imgContainer = $('.bgcoverize'),
+	    $bg          = $(".bgcoverize > .imgwrap > img"),
+	    yolo         = $bg,
+	    aspectRatio  = yolo.width() / yolo.height();
+
+	function resizeBg() {
+		$bg.each(function () {
+			yolo = $(this);
+		    aspectRatio  = yolo.width() / yolo.height();
+		    imgContainer = $(this).parent().parent();
+		    console.log(imgContainer);
+			if ( (imgContainer.width() / imgContainer.height()) < aspectRatio ) {
+			    $(this)
+			    	.removeClass('img-cover-w')
+			    	.addClass('img-cover-l');
+			} else {
+			    $(this)
+			    	.removeClass('img-cover-l')
+			    	.addClass('img-cover-w');
+			}
+		});
+
+	}
+	resizeBg();
+	$(window).resize(function () {
+		resizeBg();
+	});
+	// function imageFun(wrapper) {
+	// 	var img = $("img", wrapper);
+	// 	console.log($(wrapper));
+	// }
+	// imageFun();
+	// $(window).resize(function () {
+	// 	$('.imgwrap').each(function () {
+	// 		imageFun(this);
+	// 	});
+	// });
 });
